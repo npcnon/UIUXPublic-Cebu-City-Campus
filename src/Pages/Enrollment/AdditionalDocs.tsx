df0730a4727ba3e44681059f47197a0a8c382636
Yuli <yuliaballe2@gmail.com>
2024-07-10 13:07:32 +0800

    Delete the image on additional documents

--- src/Pages/Enrollment/AdditionalDocs.tsx
+++ src/Pages/Enrollment/AdditionalDocs.tsx
@@ -4,8 +4,7 @@ import { styled } from '@mui/system';
 //imports
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
+
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import ImageIcon from '@mui/icons-material/Image';
 //end of imports
@@ -42,25 +41,7 @@ export default function AdditionalDocs() {
       mt: 2,
       mb: 3
     }}>
-        <FormGrid item xs={12} md={6} lg={6}>
-        <Box
-      sx={{
         
-        bgcolor: '#f5f5f5',
-        p: 8,
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <Skeleton
-        sx={{ bgcolor: 'grey.900' }}
-        variant="rectangular"
-        width={210}
-        height={118}
-      />
-    </Box>
-    </FormGrid>
  
     <FormGrid item xs={12} md={6} lg={4}>     
     <Button sx={{
