94c013fa48327e11f620f1d5dfd7a069180396a9
non <afkmhafric@gmail.com>
2024-09-03 05:07:10 +0800

    fixed idservice year null possibility🧩🧩

--- src/services/studentIdService.ts
+++ src/services/studentIdService.ts
@@ -15,9 +15,12 @@ export const fetchLatestStudentId = async (): Promise<string> => {
     const response = await axios.get(`${API_URL}/stdntpersonal/?latest=true`);
     if (response.status === 200) {
       const students = response.data;
-      const yearEntry = useAcademicStore.getState().academicBackground.yearEntry.toString().slice(-2); // Use the last 2 digits of the year
-      const departmentId = useAcademicStore.getState().academicBackground.department;
-      if(students.length === 0){
+      const academicBackground = useAcademicStore.getState().academicBackground;
+      const yearEntry = academicBackground?.yearEntry
+        ? academicBackground.yearEntry.toString().slice(-2)
+        : null;
+            const departmentId = useAcademicStore.getState().academicBackground.department;
+      if(students.length === 0 && yearEntry!== null){
         console.log(`service---yearentry: ${yearEntry}-----departmentid${departmentId}`)
         return `${yearEntry.slice(-2)}${departmentId}${"0001"}`;
       }
